3eada75272646b415ed739db3c134901c39637fa
DANI <daniel.caso@ariwonto.com>
2020-11-16 03:13:45 -0500

    add comments

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        # role by default patient
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
