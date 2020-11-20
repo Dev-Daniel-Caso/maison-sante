9311345aec629145c71474cd40c5c0ddb4df5c72
DANI <daniel.caso@ariwonto.com>
2020-11-20 05:10:53 -0500

    add controller Appointment 1

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -10,13 +10,44 @@ use App\Http\Requests\StoreAppointment;
 
 class AppointmentController extends Controller
 {
-    public function index()
+	public function index()
     {
     	$user = Auth::guard('api')->user();
-    	return  $user->asPatientAppointments();
+    	$appointments = $user->asPatientAppointments()
+	    	->with([
+	    		'specialty' => function ($query) {
+	    			$query->select('id', 'name');
+	    		}, 
+	    		'doctor' => function ($query) {
+	    			$query->select('id', 'name');
+	    		}
+	    	])
+	    	->get([
+				"id",
+				"description",
+				"specialty_id",
+				"doctor_id",
+				"schedule_date",
+				"schedule_time",
+				"type",
+				"created_at",
+				"status"
+	    	]);
+
+    	return $appointments;
     }
 
     public function store(StoreAppointment $request)
     {
+    	$patientId = Auth::guard('api')->id();
+    	
+    	$appointment = Appointment::createForPatient($request, $patientId);
+    	
+    	if ($appointment) 
+    		$success = true;
+    	else 
+    		$success = false;
+
+    	return compact('success');
     }
 }
